dcb49a6eb9deea1ce94bfe1eb42dac42b655c3f6
Aakash Kumar <kumar_aakash86@yahoo.in>
2018-02-19 11:49:03 +0000

    Removing ignored files

--- ak-universal-helper/src/main/java/ak/andro/kumaraakash86/akuniversalhelper/Listeners/shake/AKShakeListener.java
+++
@@ -1,9 +0,0 @@
-package ak.andro.kumaraakash86.akuniversalhelper.Listeners.shake;
-
-/**
- * Created by AAKASH on 15-12-2016.
- */
-public interface AKShakeListener {
-
-    void shakeDetected(boolean shaked);
-}
